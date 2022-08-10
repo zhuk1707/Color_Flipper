a4bc6a7f259373735fb1a79d0a59731c5551be62
Web Monkey <104577087+zhuk1707@users.noreply.github.com>
2022-08-10 14:20:14 +0300

    added mode switcher and color generation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-let colorNames = [
+const colorNames = [
   'IndianRed', 'LightCoral', 'Salmon',
   'DarkSalmon', 'LightSalmon', 'Crimson',
   'Red', 'FireBrick', 'DarkRed',
@@ -9,15 +9,67 @@ let colorNames = [
   'Gold', 'Yellow', 'LightYellow',
   'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip',
   'Moccasin', 'PeachPuff', 'PaleGoldenrod',
-  'Khaki', 'DarkKhaki'
+  'Khaki', 'DarkKhaki', 'Lavender',
+  'Thistle', 'Plum', 'Violet',
+  'Orchid', 'Fuchsia', 'Magenta',
+  'MediumOrchid', 'MediumPurple',  'BlueViolet', 
+  'DarkViolet',  'DarkOrchid',  'DarkMagenta',
+  'Purple', 'Indigo',  'SlateBlue',
+  'DarkSlateBlue'
 ]
 
-function generateColor() {
-  let randomValue = Math.floor(Math.random() * colorNames.length)
+const hex = [
+  1, 2, 3, 4, 5, 6, 7, 8,
+  9, 'A', 'B', 'C', 'D', 'E', 'F'
+]
 
-  let generatedColor = colorNames[randomValue]
+let mode = "simple"
+let modeButtons = document.querySelectorAll('.mode-btn')
+let simpleBtn = document.querySelector('.panel__btn_left')
+let hexBtn = document.querySelector('.panel__btn_right')
+
+modeButtons.forEach( (button) => {
+  button.addEventListener("click", (e) => {
+    if (e.currentTarget.innerText === "Simple") {
+      mode = 'simple'
+    }
+    else {
+      mode = 'hex'
+    }
+
+    if (mode === 'simple') {
+      simpleBtn.classList.toggle('active')
+      hexBtn.classList.toggle('active')
+      generateColor()
+    } else {
+      simpleBtn.classList.toggle('active')
+      hexBtn.classList.toggle('active')
+      generateColor()
+
+    }
+  })
+  
+})
+
+function getGeneratedColor(arr) {
+  let randomValue
+  randomValue = Math.floor(Math.random() * arr.length)
+  return arr[randomValue]
+}
+
+function generateColor() {
+  let generatedColor
+  
+  if (mode === 'simple') {
+    generatedColor = getGeneratedColor(colorNames)
+    
+  } else {
+    generatedColor = "#"
+    for (let i = 0; i < 6; i++) {
+      generatedColor += getGeneratedColor(hex)
+    }
+  }
   
   document.querySelector('.panel__info span').innerHTML = generatedColor
-  
   document.body.style.backgroundColor = generatedColor
 }
